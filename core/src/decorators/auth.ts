38c49f72927320ccb58bf497d3fb6699160194a0
CallbacKiran <bhaleraok65@gmail.com>
2021-07-15 20:03:58 +0530

    update authchecker, support for typecasting on req.params

--- core/src/decorators/auth.ts
+++ core/src/decorators/auth.ts
@@ -1,24 +1,34 @@
-import { NextFunction, Request, Response } from 'express'
-import { CustomError } from '../classes/error'
-import { createThrowable } from '../utils/createThrowable'
+import { NextFunction, Request, Response } from "express";
+import { CustomError } from "../classes/error";
+import { createThrowable } from "../utils/createThrowable";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type U = any
+type U = any;
 
 interface AuthType<T extends string> {
-  role?: T | T[]
-  error?: string
-  extends?: AuthExtender<T>
+  role?: T | T[];
+  error?: string;
+  extends?: AuthExtender<T>;
 }
 
-const defaultUserTypeResolver = (req: U) => req.user?.userType
-const defaultAuthChecker = (_: Request, acceptedRole: string[], currentRole?: string) => {
-  if (!currentRole) {
-    return false
+const defaultUserTypeResolver = (req: U) => req.user?.userType;
+const defaultAuthChecker = (
+  req: U,
+  acceptedRole?: string[],
+  currentRole?: string
+) => {
+  if (!acceptedRole) {
+    // acceptedRole is not provided means there is no role based authorization
+    // simply check for current user in req object
+    return !!req.user;
+  } else {
+    if (!currentRole) {
+      return false;
+    }
+
+    return acceptedRole.includes(currentRole);
   }
-
-  return acceptedRole.includes(currentRole)
-}
+};
 
 /**
  * 🔒 Method must be async route handler
@@ -26,52 +36,64 @@ const defaultAuthChecker = (_: Request, acceptedRole: string[], currentRole?: st
  */
 export function Auth<T extends string>(props?: AuthType<T>) {
   return (_target: U, _key: string, descriptor: PropertyDescriptor) => {
-    const originalMethod = descriptor.value
+    const originalMethod = descriptor.value;
 
-    if (typeof originalMethod !== 'function') {
-      throw new SyntaxError('Invalid use of @Auth Decorator')
+    if (typeof originalMethod !== "function") {
+      throw new SyntaxError("Invalid use of @Auth Decorator");
     }
 
-    descriptor.value = async function (req: Request, res: Response, next: NextFunction) {
+    descriptor.value = async function (
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) {
       /** Error handler */
-      const throwable = createThrowable(req, res, next)
+      const throwable = createThrowable(req, res, next);
 
-      const role = props?.extends?.props?.role || props?.role
-      const error = props?.extends?.props?.error || props?.error || 'Invalid Permissions'
-      const userTypeResolver = props?.extends?.userTypeResolver || defaultUserTypeResolver
-      const currentUserType = userTypeResolver(req)
+      const role = props?.extends?.props?.role || props?.role;
+      const error =
+        props?.extends?.props?.error || props?.error || "Invalid Permissions";
+      const userTypeResolver =
+        props?.extends?.userTypeResolver || defaultUserTypeResolver;
+      const authChecker = props?.extends?.authChecker || defaultAuthChecker;
+
+      const currentUserType = userTypeResolver(req);
 
       if (role) {
-        const roles = [role].flat()
-
-        const authChecker = props?.extends?.authChecker || defaultAuthChecker
+        const roles = [role].flat();
 
         if (!authChecker(req, roles, currentUserType)) {
-          return next(new CustomError(error))
+          return next(new CustomError(error));
         }
-      } else if (!currentUserType) {
-        return next(new CustomError(error))
+      } else if (!authChecker(req)) {
+        return next(new CustomError(error));
       }
 
-      await throwable(async () => await originalMethod.apply(this, [req, res, next]))
-    }
+      await throwable(
+        async () => await originalMethod.apply(this, [req, res, next])
+      );
+    };
 
-    return descriptor
-  }
+    return descriptor;
+  };
 }
 
 abstract class AuthExtender<T extends string> {
   /** `userTypeResolver(req: Request): string | undefined` */
   public userTypeResolver(req: Request) {
-    return defaultUserTypeResolver(req)
+    return defaultUserTypeResolver(req);
   }
 
   /** `authChecker(req: Request, acceptedRole: string[], currentRole?: string | undefined): boolean` */
-  public authChecker(req: Request, acceptedRole: string[], currentRole?: string) {
-    return defaultAuthChecker(req, acceptedRole, currentRole)
+  public authChecker(
+    req: Request,
+    acceptedRole?: string[],
+    currentRole?: string
+  ) {
+    return defaultAuthChecker(req, acceptedRole, currentRole);
   }
 
   constructor(public props?: { role: T | T[]; error?: string }) {}
 }
 
-Auth.extender = AuthExtender
+Auth.extender = AuthExtender;
--- core/src/decorators/controller.ts
+++ core/src/decorators/controller.ts
@@ -1,77 +1,183 @@
-import express, { Router } from 'express'
-import { ClassType } from '../classes/injector'
-import { RoutePropType } from '../decorators/route'
+import express, { Request, Router } from "express";
+import { ClassType } from "../classes/injector";
+import { RoutePropType } from "../decorators/route";
 
 export interface Controller {
-  readonly path: string
-  readonly router: Router
+  readonly path: string;
+  readonly router: Router;
 }
 
+type ParamDict = {
+  [key: string]: string | number | boolean;
+};
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type U = any
+type U = any;
 
 /** Append slash(/) to the given string, if it doesn't have */
-function appendSlash(str = '') {
-  if (str.length > 1 && !str.startsWith('/')) {
-    str = `/${str}`
+function appendSlash(str = "") {
+  if (str.length > 1 && !str.startsWith("/")) {
+    str = `/${str}`;
   }
 
-  return str
+  return str;
+}
+
+const supportedPipes = ["|number", "|string", "|boolean"] as const;
+type PipeType = typeof supportedPipes[number];
+
+// "/a/:num|number" here outcome will be /a/:num
+function removeParamPipes(path: string | RegExp) {
+  if (typeof path === "string") {
+    supportedPipes.forEach((pipe) => {
+      if (typeof path === "string") {
+        path = path.replace(pipe, "");
+      }
+    });
+  }
+
+  return path;
+}
+
+// "/a/:num|number"
+function detectTypeWithPipe(path: string | RegExp) {
+  const params: { param: string; typ: PipeType }[] = [];
+
+  if (typeof path === "string") {
+    path
+      .split("/")
+      .filter((p) => p[0] === ":")
+      .map((p) => {
+        const paraT = p.split("|");
+
+        if (paraT.length === 2) {
+          let pipe = paraT[1];
+          const typ = `|${pipe}` as PipeType;
+
+          if (supportedPipes.includes(typ)) {
+            params.push({
+              param: paraT[0].slice(1),
+              typ,
+            });
+          }
+        }
+      });
+
+    path = removeParamPipes(path);
+
+    if (params.length > 0) {
+      return { path, params };
+    }
+
+    return null;
+  }
+
+  return null;
+}
+
+function convertParamTypeWithSupportedPipes(
+  param: ParamDict[keyof ParamDict],
+  typ: PipeType
+) {
+  switch (typ) {
+    case "|string":
+      return String(param);
+    case "|number":
+      return Number(param);
+    case "|boolean":
+      return param === "true";
+    default:
+      return param;
+  }
 }
 
 /** check if given input is string or RegExp, if it's string then pass it to appendSlash*/
 function withSlash(input: string | RegExp) {
-  if (typeof input === 'string') {
-    return appendSlash(input)
+  if (typeof input === "string") {
+    return appendSlash(input);
   }
 
-  return input
+  return input;
 }
 
 export function isInstanceOfController(instance: U): instance is Controller {
-  return typeof instance.path === 'string' && !!instance.router && Object.getPrototypeOf(instance.router) === Router
+  return (
+    typeof instance.path === "string" &&
+    !!instance.router &&
+    Object.getPrototypeOf(instance.router) === Router
+  );
 }
 
-export function Controller(path = '') {
+export function Controller(path = "") {
   return function <T extends ClassType<U>>(Target: T) {
     // Here `implements Controller` is redundant b'cuz of the typescript issue as mention bellow
     // https://github.com/microsoft/TypeScript/issues/4881 😔
     // class extends Target implements Controller {
 
     return class extends Target {
-      public readonly path = path
-      public readonly router: Router
+      public readonly path = path;
+      public readonly router: Router;
 
       constructor(...args: U[]) {
-        super(...args)
-        Object.setPrototypeOf(this, Target.prototype)
+        super(...args);
+        Object.setPrototypeOf(this, Target.prototype);
 
         // create instance of Express Router
-        this.router = express.Router()
+        this.router = express.Router();
 
         // figure out the route handlers
-        const RouteHandlers = Object.getOwnPropertyNames(Object.getPrototypeOf(this)).filter((method) => {
-          return Reflect.hasMetadata(`${this.constructor.name}:::${method}`, this)
-        })
+        const RouteHandlers = Object.getOwnPropertyNames(
+          Object.getPrototypeOf(this)
+        ).filter((method) => {
+          return Reflect.hasMetadata(
+            `${this.constructor.name}:::${method}`,
+            this
+          );
+        });
 
         for (const routeHandler of RouteHandlers) {
           // Collect the metadata
-          const option: Required<RoutePropType> = Reflect.getMetadata(`${Target.name}:::${routeHandler}`, this)
+          const option: Required<RoutePropType> = Reflect.getMetadata(
+            `${Target.name}:::${routeHandler}`,
+            this
+          );
 
           // handler
-          const handler = this[routeHandler]
+          const handler = this[routeHandler];
 
           // check for slash(/)
-          let path = option['PATH']
-          path = [path].flat().map(withSlash)
+          let path: any = option["PATH"];
+          const oldPath = [path].flat().map(withSlash);
+
+          path = [...oldPath].map(removeParamPipes);
 
           // call the get, post ... of Express Router with `path` and `handler`
-          this.router[option['METHOD']](path, (...args) => handler.call(this, ...args))
+          this.router[option["METHOD"]](
+            path,
+            (req: Request<ParamDict>, res, next) => {
+              // make typed params
+              oldPath
+                .map(detectTypeWithPipe)
+                .filter(Boolean)
+                .forEach((pt) => {
+                  pt?.params.forEach(({ param, typ }) => {
+                    if (req.params[param]) {
+                      req.params[param] = convertParamTypeWithSupportedPipes(
+                        req.params[param],
+                        typ
+                      );
+                    }
+                  });
+                });
+
+              return handler.call(this, req, res, next);
+            }
+          );
 
           // Now clear the metadata (/The work is done, It always will be --Thanos 🎃/)
-          Reflect.deleteMetadata(`${Target.name}:::${routeHandler}`, this)
+          Reflect.deleteMetadata(`${Target.name}:::${routeHandler}`, this);
         }
       }
-    }
-  }
+    };
+  };
 }
